2c25bab78d559a7d4ec554475a59a35daca0b712
Glenn Sarti <glenn.sarti@bankwest.com.au>
2014-07-05 18:38:07 +0800

    Added script parameters Added sanity checks to make sure script params are ok Added an extra XML element with the status id in Write-ResourceStatusSummary function.  Makes it easier to reference it as it does not contain spaces Added an extra XML element in Write-HashTableToXML.  Whenever an element called 'evaluation_time' is found, it also generates an 'evaluation_time_ms' element with the duration expressed as milliseconds.  Easier for TeamCity reports as it's measured in ms Wrote the conversion logic to use the script parameters The script params accept strings as well as an array of strings

--- parsereport.ps1
+++ parsereport.ps1
@@ -1,23 +1,34 @@
-param()
-
-# References
-# Report v3/4 Reference for Puppet 2.7.x and 3.x
-# http://docs.puppetlabs.com/puppet/3/reference/format_report.html
+param([string[]]$Report = '', [string[]]$Transform = '', [string]$OutputDir = '', [switch]$OutputXML = $false)
 
 $ErrorActionPreference = "Stop"
 $DebugPreference = "SilentlyContinue"
 $VerbosePreference = "SilentlyContinue"
 
+# References
+# Report v3/4 Reference for Puppet 2.7.x and 3.x
+# http://docs.puppetlabs.com/puppet/3/reference/format_report.html
+
+# Fatal Sanity Checks
+if ($Report -eq '') { Throw 'No puppet report file was specified' }
+$Report | % {
+  if (!(Test-Path $_)) { Throw ('Report file ' + $_ + ' does not exist') }
+}
+if ($Transform -eq '') { Throw 'No transform was specified' }
+$Transform | % {
+  if (!(Test-Path "$PSScriptRoot\transforms\$($_).xsl")) { Throw ('Transform file ' + $_ + ' does not exist') }
+}
+if ($OutputDir -ne '') {
+  if (!(Test-Path $OutputDir)) { Throw ('Output directory of ' + $OutputDir + ' does not exist') }
+}
+if (!(Test-Path "$PSScriptRoot\poweryaml\PowerYaml.psm1")) { Throw 'Could not find the PowerYaml module file' }
+
+# Load required modules
+Write-Debug 'Removing PowerYaml Module if it is already loaded...'
 Get-Module | Where-Object { $_.Name -eq 'PowerYaml' } | Remove-Module
+Write-Debug 'Loading the PowerYaml module'
 Import-Module "$PSScriptRoot\poweryaml\PowerYaml.psm1"
 
-$yamlFile = "$PSScriptRoot\examples\201405200656.yaml"
-$transformFile = "$PSScriptRoot\transforms\html.basicreport.xsl"
-
-$objYaml = Get-Yaml -FromFile $yamlfile
-
-$xmlDoc = [xml]"<report />"
-
+# Function definitions
 function Transform-XML($xmlDocument, $transformFilename) {
 	$xmlContentReader = ([System.Xml.XmlReader]::Create( (New-Object System.IO.StringReader($xmlDocument.innerXML))))
 
@@ -46,6 +57,7 @@ function Write-ResourceStatusSummary($objYaml, $xmlDoc) {
   $noChange = 0
   $total = 0
   for($index = $resourceMetrics.GetLowerBound(0); $index -lt $resourceMetrics.GetUpperBound(0); $index = $index + 3) {
+    
     if ($resourceMetrics[$index] -eq 'total')
     {
       $noChange = $noChange + $resourceMetrics[$index + 2]
@@ -54,10 +66,17 @@ function Write-ResourceStatusSummary($objYaml, $xmlDoc) {
     else
     {
       $noChange = $noChange - $resourceMetrics[$index + 2]
-      $statuses[$resourceMetrics[$index + 1]] = $resourceMetrics[$index + 2]
+
+      $statusInfo = @{}
+      $statusInfo['count'] = $resourceMetrics[$index + 2]
+      $statusInfo['id'] = $resourceMetrics[$index]
+      $statuses[$resourceMetrics[$index + 1]] = $statusInfo
     } 
   }
-  $statuses["No change"] = $noChange
+  $statusInfo = @{}
+  $statusInfo['count'] = $noChange
+  $statusInfo['id'] = 'nochange'
+  $statuses["No change"] = $statusInfo
   
   $summaryNode = $xmlDoc.createElement('resourcesummary')
   [void]($summaryNode.SetAttribute('total',$total))
@@ -65,9 +84,10 @@ function Write-ResourceStatusSummary($objYaml, $xmlDoc) {
   $statuses.Keys | ForEach-Object {
     $node = $xmlDoc.createElement('status')
     $node.SetAttribute('name',$_)
-    $node.SetAttribute('count',$statuses[$_])
+    $node.SetAttribute('id',$statuses[$_].id)
+    $node.SetAttribute('count',$statuses[$_].count)
     if ($total -gt 0)
-    { $node.SetAttribute('percent',[int](($statuses[$_] / $total)*100)) }
+    { $node.SetAttribute('percent',[int](($statuses[$_].count / $total)*100)) }
     else
     { $node.SetAttribute('percent',0) }
 
@@ -81,14 +101,30 @@ function Write-HashTableToXML($attrName, $attrValue, $rootNode) {
   $xmlNode = $rootNode.OwnerDocument.createElement($attrName)
   if ($attrValue -ne $null) {
     switch ($attrValue.GetType().ToString()) {
-      'System.String' { $xmlNode.innerText = $attrValue; break ; }
-      'System.Object[]' { $xmlNode.innerText = $attrValue; break ; }
+      'System.String' {
+        # Special property.  Convert it to ms as well
+        if ($attrName -eq 'evaluation_time') {
+          $xmlMSnode = $rootNode.OwnerDocument.createElement($attrName + '_ms')
+          $xmlMSnode.innerText = [int](([float]$attrValue)*1000)
+          [void]($rootNode.appendChild($xmlMSnode))
+        }
+        $xmlNode.innerText = $attrValue;
+        break ;
+      }
+      'System.Object[]' {
+        if ( ($attrValue[0]).GetType().ToString() -eq 'System.Collections.HashTable' ) {
+          $attrValue | % { Write-HashTableToXML $attrName $_ $xmlNode }
+        } else {
+          $xmlNode.innerText = $attrValue;
+        }
+        break ;
+      }
       'System.Collections.Hashtable' { $attrValue.Keys | % { Write-HashTableToXML $_ $attrValue[$_] $xmlNode }; break ; }
       default { $xmlNode.innerText = ('Unknown type ' + $attrValue.GetType().ToString()); break ; }
     }
     
   }
-  [void]($rootNode.appendChild($xmlNode))  
+  [void]($rootNode.appendChild($xmlNode))
 }
 
 function Write-ResourceStatus($objYaml, $xmlDoc) {
@@ -97,7 +133,6 @@ function Write-ResourceStatus($objYaml, $xmlDoc) {
   $objYaml.resource_statuses.Keys | % {
     $resourceNode = $xmlDoc.createElement('resource')
     [void]($resourceNode.SetAttribute('name',$_))
-
     $resource = $objYaml.resource_statuses[$_]
 
     $resource.Keys | % {  
@@ -108,16 +143,38 @@ function Write-ResourceStatus($objYaml, $xmlDoc) {
   [void]($xmlDoc.DocumentElement.AppendChild($resourcesNode))
 }
 
-Write-ResourceStatusSummary $objYaml $xmlDoc
-Write-ResourceStatus $objYaml $xmlDoc
+# Process the reports and transforms...
+$Report | % {
+  $yamlFile = $_
 
+  Write-Debug "Reading $yamlFile ..."
+  $objYaml = Get-Yaml -FromFile $yamlFile
 
-##$xmlDoc.innerXml
-$xmlDoc.innerXml | Out-File "$PSScriptRoot\downloads\test.xml"
+  $xmlDoc = [xml]"<report />"
+  Write-Debug "Creating Resource Status Summary XML..."
+  Write-ResourceStatusSummary $objYaml $xmlDoc
+  
+  # TODO: Write out the Metric.Time table to determine the total time
+  
+  Write-Debug "Creating Resource Status XML..."
+  Write-ResourceStatus $objYaml $xmlDoc
 
-Transform-XML -XMLDocument $xmlDoc -transformFilename $transformFile | Out-File "$PSScriptRoot\downloads\test.html"
+  if ($OutputXML) {    
+    $filename = Join-Path -Path $OutputDir -ChildPath ( (Get-ChildItem $yamlFile).BaseName + '.xml')
+    Write-Debug "Writing XML to $filename ..."
+    $xmlDoc.innerXml | Out-File $filename -Encoding ASCII -Force -Confirm:$false
+  }
 
-
-
-
-
+  $Transform | % {
+    $transformFile = "$PSScriptRoot\transforms\$($_).xsl"
+    
+    if ($_.ToLower().EndsWith('.stdout')) {
+      Write-Debug "Applying transform $transformFile , output to STDOUT..."
+      Transform-XML -XMLDocument $xmlDoc -transformFilename $transformFile
+    } else {
+      $filename = Join-Path -Path $OutputDir -ChildPath ( (Get-ChildItem $yamlFile).BaseName + '.' + $_)
+      Write-Debug "Applying transform $transformFile , output to $filename ..."
+      Transform-XML -XMLDocument $xmlDoc -transformFilename $transformFile | Out-File $filename -Force -Confirm:$false 
+    }    
+  }
+}
