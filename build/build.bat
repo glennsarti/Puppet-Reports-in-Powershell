0264d3e0e85de958913e7579dc7d0868ea6df6cc
Glenn Sarti <glenn.sarti@outlook.com>
2014-09-11 21:08:24 +0800

    Updated build.bat to build a functional chocolatey package

--- build/build.bat
+++ build/build.bat
@@ -11,6 +11,7 @@ IF %THISDIR:~-1%==\ SET THISDIR=%THISDIR:~0,-1%
 
 SET WORKING=%THISDIR%\working
 SET TOOLS=%WORKING%\tools
+SET TOOLS_MODULE=%WORKING%\tools\module
 SET SRC=%THISDIR%\..\src
 
 PUSHD
@@ -19,19 +20,29 @@ CD /D %THISDIR%
 ECHO Cleaning the working directory
 RD /S /Q "%WORKING%" > NUL
 MKDIR "%WORKING%"
+ECHO Creating directory structure
+MKDIR "%TOOLS%"
+MKDIR "%TOOLS_MODULE%"
 
 ECHO Copy source ready for packing...
-MKDIR "%TOOLS%"
-XCOPY "%SRC%" "%TOOLS%" /s /e /v /c /i /f /y
+XCOPY "%SRC%" "%TOOLS_MODULE%" /s /e /v /c /i /f /y
 
 ECHO Removing extra files...
-DEL "%WORKING%\Tools\InstallPowerYamlGithub.cmd"
+DEL "%TOOLS_MODULE%\InstallPowerYamlGithub.cmd"
 
-SET PKGVERSION=%APPVEYOR_BUILD_VERSION%
-IF NOT [%PKGVERSION%] == [] SET PKGVERSION=-Version "0.9.%PKGVERSION%"
+ECHO Copying chocolatey support files...
+COPY "%THISDIR%\chocolateyInstall.ps1" "%TOOLS%\chocolateyInstall.ps1"
+COPY "%THISDIR%\chocolateyUninstall.ps1" "%TOOLS%\chocolateyUninstall.ps1"
+
+ECHO Getting the package version from various sources
+SET PKGVERSION=0.0.1
+IF NOT [%1] == [] SET PKGVERSION=%1
+IF NOT [%APPVEYOR_BUILD_VERSION%] == [] SET PKGVERSION=0.9.%APPVEYOR_BUILD_VERSION%
 ECHO Using package version %PKGVERSION%
+IF NOT [%PKGVERSION%] == [] SET PKGVERSION=-Version "%PKGVERSION%"
 
 ECHO Run Nuget Pack
-"nuget.exe" pack  "%~dp0Package.nuspec" -OutputDirectory "%THISDIR%" -BasePath "%WORKING%" -NonInteractive %PKGVERSION%
+COPY "%THISDIR%\Package.nuspec" "%WORKING%\Package.nuspec"
+"nuget.exe" pack  "%WORKING%\Package.nuspec" -NonInteractive %PKGVERSION% -NoPackageAnalysis
 
 EXIT /B %ERRORLEVEL%
