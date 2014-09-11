0a67f03c02ce63b3ea3c7c9bdf8724688d81d315
Glenn Sarti <glenn.sarti@outlook.com>
2014-09-11 21:09:02 +0800

    Added chocolatey install and uninstall scripts to put/remove the modules into the default Powershell Module location

--- build/chocolateyInstall.ps1
+++ build/chocolateyInstall.ps1
@@ -1,1 +1,16 @@
-# TODO Do something to install this package 
+$packageName = 'poshpuppetreports' # arbitrary name for the package, used in messages
+
+try { 
+  $installDir = Join-Path (Join-Path $PSHome "Modules") "POSHPuppetReports"
+
+  $sourceDir = Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) "module"
+
+  # Copy the folder to the powershell modules directory
+  [void] (Copy-Item -Path $sourceDir -Destination $installDir -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
+     
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
+
--- build/chocolateyUninstall.ps1
+++ build/chocolateyUninstall.ps1
@@ -1,1 +1,14 @@
-# TODO Do something to uninstall this package 
+$packageName = 'poshpuppetreports' # arbitrary name for the package, used in messages
+
+try { 
+  $installDir = Join-Path (Join-Path $PSHome "Modules") "POSHPuppetReports"
+
+  # Remove the folder from the powershell modules directory
+  [void] (Remove-Item $installDir -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
+     
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
+
