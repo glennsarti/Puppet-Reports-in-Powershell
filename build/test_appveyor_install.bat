e034bf79740dde937e5a7474681111d25594e3d5
Glenn Sarti <glenn.sarti@outlook.com>
2014-09-14 09:06:33 +0800

    Added test install script to use the appveyor ci nuget feed.  This can be used to install the latest build, whereas the public myget feed would be used for the stable builds Modified the uninstall script as it doesn't require a source

--- build/test_appveyor_install.bat
+++ build/test_appveyor_install.bat
@@ -5,5 +5,5 @@ REM Does string have a trailing slash? if so remove it
 SET THISDIR=%~dp0
 IF %THISDIR:~-1%==\ SET THISDIR=%THISDIR:~0,-1%
 
-ECHO Uninstalling...
-CUNINST poshpuppetreports
+ECHO Installing...
+CINST poshpuppetreports -source https://ci.appveyor.com/nuget/puppet-reports-in-powershell-jssds0kkdns6
