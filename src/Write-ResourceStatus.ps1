66c5a99d35dba27be6e6bc6ffbceb1ee702b3900
Glenn Sarti <glenn.sarti@outlook.com>
2014-09-17 21:54:57 +0800

    The events resource is special as it can come in as null, an array or hashtable. I changed the rendering based on the object type.  So now there will always be one 'events' xml node with zero or more 'event' nodes as children.  This makes the stylesheet much easier to write and makes more sense in the long run.

--- src/Write-ResourceStatus.ps1
+++ src/Write-ResourceStatus.ps1
@@ -6,8 +6,33 @@ function Write-ResourceStatus($objYaml, $xmlDoc) {
     [void]($resourceNode.SetAttribute('name',$_))
     $resource = $objYaml.resource_statuses[$_]
 
-    $resource.Keys | % {  
-      Write-HashTableToXML $_ ($resource[$_]) $resourceNode
+    $resource.Keys | % {
+      $eventsObject = $resource[$_]
+      if ($_ -eq 'events')
+      {
+        # The events resource is special as it can come in as null, an array or hashtable. Need to tailor the XML rendering based on object type
+        $eventsNode = $xmlDoc.createElement('events')
+        if ($eventsObject -ne $null) {        
+          switch ($eventsObject.GetType().ToString() ) {
+            "System.Collections.Hashtable" {
+              Write-HashTableToXML 'event' $eventsObject $eventsNode
+              break;
+            }
+            "System.Object[]" {
+              $eventsObject | % {
+                Write-HashTableToXML 'event' $_ $eventsNode
+              }
+              break;
+            }
+            default { Throw "Write-ResourceStatus: Unknown object type $($eventsObject.GetType().ToString())"; return $null; }
+          }
+        }
+        [void]($resourceNode.AppendChild($eventsNode))
+      }
+      else
+      {    
+        Write-HashTableToXML $_ $eventsObject $resourceNode
+      }
     }
     [void]($resourcesNode.AppendChild($resourceNode))
   }
