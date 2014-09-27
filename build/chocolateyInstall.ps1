c333d473a8f0798a3c82fd8c1d81414dadc687d5
Glenn Sarti <glenn.sarti@outlook.com>
2014-09-27 20:53:36 +0800

    Added creating a shortcut called "Puppet Report GUI" to the 'Start Menu'  This functionality only works in .Net Framework 4.0 or above for the moment.

--- build/chocolateyInstall.ps1
+++ build/chocolateyInstall.ps1
@@ -7,7 +7,24 @@ try {
 
   # Copy the folder to the powershell modules directory
   [void] (Copy-Item -Path $sourceDir -Destination $installDir -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
-     
+  
+  # Create a shortcut for the reports into the "Start Menu"
+  $shortcutPath = ([Environment]::GetFolderPath('CommonStartMenu')) + '\POSH Puppet Reports\Report GUI.lnk'
+  if (! (Test-Path $shortcutPath)) {
+    Write-Debug 'Creating shortcut folder...'
+    [void](New-Item -Path $shortcutPath -ItemType Directory -Force)
+  }
+  $shortcutFile = $shortcutPath + '\Puppet Report GUI.lnk'  
+  $targetPath = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe'  
+
+  # TODO: Waiting on FerventCoder to release new chocolatey package with the Install-ChocolateyShortcut function available. Until then, use old-school WScript.
+  #Install-ChocolateyShortcut -shortcutFilePath $shortcutPath -targetPath $targetPath  
+  $WshShell = New-Object -ComObject WScript.Shell
+  $Shortcut = $WshShell.CreateShortcut($shortcutFile)
+  $Shortcut.TargetPath = $targetPath
+  $Shortcut.Arguments = "`"& { . '$($installDir)\reportgui\reportgui.ps1'}`""
+  $Shortcut.Save()
+    
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
--- build/chocolateyUninstall.ps1
+++ build/chocolateyUninstall.ps1
@@ -4,8 +4,20 @@ try {
   $installDir = Join-Path (Join-Path $PSHome "Modules") "POSHPuppetReports"
 
   # Remove the folder from the powershell modules directory
-  [void] (Remove-Item $installDir -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
-     
+  if (Test-Path  $installDir) {
+    [void] (Remove-Item $installDir -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
+  }
+  else
+  {
+    Write-Debug "Installation directory doesn't exist"
+  }
+
+  # Removing shortcuts for the reports into the "Start Menu"
+  $shortcutPath = ([Environment]::GetFolderPath('CommonStartMenu')) + '\POSH Puppet Reports\Report GUI.lnk'
+  if (Test-Path $shortcutPath) {
+    [void] (Remove-Item $shortcutPath -Recurse -Confirm:$false -Force -ErrorAction 'Stop')
+  }
+
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
