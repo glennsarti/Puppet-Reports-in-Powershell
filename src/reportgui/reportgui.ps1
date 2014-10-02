782a974e6b0cc31e4a64d6503b80f56628bcb5a9
Glenn Sarti <glenn.sarti@outlook.com>
2014-10-02 20:08:54 +0800

    Changed the way the report folder is specified.  Just changed it to a basic text box where you can either enter the path directly or browse to it

--- src/reportgui/reportgui.ps1
+++ src/reportgui/reportgui.ps1
@@ -18,6 +18,8 @@ Write-Verbose 'Loading WPF assemblies'
 Add-Type –assemblyName PresentationFramework
 Add-Type –assemblyName PresentationCore
 Add-Type –assemblyName WindowsBase
+Write-Verbose 'Loading Windows Forms assemblies'
+Add-Type -AssemblyName System.Windows.Forms
 Write-Verbose 'Loading the POSHPuppetReports module'
 Import-Module "$PSScriptRoot\..\POSHPuppetReports.psd1"
 
@@ -38,31 +40,54 @@ $Global:wpfWindow = [Windows.Markup.XamlReader]::Load($reader)
 
 # Wire up the XAML
 Write-Verbose "Adding XAML event handlers..."
-(Get-WPFControl 'buttonConnect').Add_Click({
-  $serverName = ((Get-WPFControl 'textServerName').Text)
-  $userName = ((Get-WPFControl 'textUsername').Text)
-  $location = ((Get-WPFControl 'textReportLocation').Text)
-      
-  [void]( Open-Location -ServerName $serverName -ConnectAs $userName -TargetDirectory $location )
-
-  # Populate the list box
-  Write-Verbose 'Populating the report list...'
-  [xml]$xmlDoc = '<reports xmlns=""></reports>'
- 	Get-Item -Path 'PuppetReports:\*.yaml' | Sort-Object ($_.LastWriteTime) -Descending | % {
- 	  $xmlNode = $xmlDoc.CreateElement('report')
- 	  $xmlNode.SetAttribute('name',$_.name.ToString())
- 	  $xmlNode.SetAttribute('datemodified',($_.LastWriteTime.ToString('dd MMM yyyy HH:mm:ss')))
- 	  $xmlNode.innerText = ($_.FullName)
- 	  $xmlDoc.reports.AppendChild($xmlNode)
+(Get-WPFControl 'buttonBrowseReportPath').Add_Click({
+  $dialogWindow = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
+    SelectedPath = (Get-WPFControl 'textReportPath').Text;
+    ShowNewFolderButton = $false;
+    Description = "Browse for Puppet Report path";
   }
-  # Write the xml document to the XAML for databinding
-  (Get-WPFControl 'xmlReportList').Document = $xmlDoc
-
-  Write-Verbose 'Expanding the report list'
-  # Expand the Reports List
-  (Get-WPFControl 'expandReportList').IsExpanded = $true   
-  # Contract the Report Location
-  (Get-WPFControl 'expandReportLocation').IsExpanded = $false
+  
+  $result = $dialogWindow.ShowDialog()
+  
+  if ($result.ToString() -eq 'Ok') {
+    (Get-WPFControl 'textReportPath').Text = $dialogWindow.SelectedPath
+  }
+})
+(Get-WPFControl 'buttonConnect').Add_Click({
+  $location = ((Get-WPFControl 'textReportPath').Text)
+  
+  if (Test-Path -Path $location) {
+    # Remove the PuppetReports Drive if it exists...
+    (Get-PSDrive 'PuppetReports' -ErrorAction 'SilentlyContinue' | Remove-PSDrive)
+    # Create a PuppetReports: drive
+    [void](New-PSDrive -Name 'PuppetReports' -PSProvider FileSystem -Root $location -Scope Script)
+  
+    # Populate the list box
+    Write-Verbose 'Populating the report list...'
+    [xml]$xmlDoc = '<reports xmlns=""></reports>'
+   	Get-Item -Path 'PuppetReports:\*.yaml' | Sort-Object ($_.LastWriteTime) -Descending | % {
+   	  $xmlNode = $xmlDoc.CreateElement('report')
+   	  $xmlNode.SetAttribute('name',$_.name.ToString())
+   	  $xmlNode.SetAttribute('datemodified',($_.LastWriteTime.ToString('dd MMM yyyy HH:mm:ss')))
+   	  $xmlNode.innerText = ($_.FullName)
+   	  $xmlDoc.reports.AppendChild($xmlNode)
+    }
+    # Write the xml document to the XAML for databinding
+    (Get-WPFControl 'xmlReportList').Document = $xmlDoc
+  
+    Write-Verbose 'Expanding the report list'
+    # Expand the Reports List
+    (Get-WPFControl 'expandReportList').IsExpanded = $true   
+    # Contract the Report Location
+    (Get-WPFControl 'expandReportLocation').IsExpanded = $false
+    
+    $wpfWindow.Title = "Puppet Report Viewer - $location"
+  }
+  else
+  {
+    [void] ([System.Windows.MessageBox]::Show('The report path does not exist','Error','Ok','Information'))
+    return
+  }
 })
 (Get-WPFControl 'listReports').Add_MouseDoubleClick({
   param($sender,$e)
